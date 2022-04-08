6af38ebec9d31fef0e49920f99d2ebd979677b23
Alex Gorichev <Alex.Gorichev@protonmail.com>
2022-04-08 22:23:41 +0100

    Reformat (and use tabs like a reasonable person)

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,25 @@
 fn main() {
-    use std::io::Write;
-    use markdown::file_to_html;
-    use html_editor::{parse, Selector, prelude::{Editable, Htmlifiable}};
+	use html_editor::{parse, prelude::{Editable, Htmlifiable}, Selector};
+	use markdown::file_to_html;
+	use std::io::Write;
 
-    let args: Vec<String> = std::env::args().collect();
-    let markdown_file = &args[1];
-    let template_file = &args[2];
-    let output_file = &args[3];
+	let args: Vec<String> = std::env::args().collect();
+	let markdown_file = &args[1];
+	let template_file = &args[2];
+	let output_file = &args[3];
 
-    // Read markdown file and convert to html, then simply read the template html file
-    let markdown_html_contents = file_to_html(std::path::Path::new(markdown_file)).unwrap();
-    let template_html_contents = std::fs::read_to_string(template_file).unwrap();
+	// Read markdown file and convert to html, then simply read the template html file
+	let markdown_html_contents = file_to_html(std::path::Path::new(markdown_file)).unwrap();
+	let template_html_contents = std::fs::read_to_string(template_file).unwrap();
 
-    let markdown_html = parse(&markdown_html_contents).unwrap();
-    let mut template_html = parse(&template_html_contents).unwrap();
-    
+	let markdown_html = parse(&markdown_html_contents).unwrap();
+	let mut template_html = parse(&template_html_contents).unwrap();
 
-    for i in markdown_html {
-        // Loop through every element in the markdown and add it to main
-        template_html.insert_to(&Selector::from("main"), i);
-    }
+	for i in markdown_html {
+		// Loop through every element in the markdown and add it to main
+		template_html.insert_to(&Selector::from("main"), i);
+	}
 
-    let mut output = std::fs::File::create(output_file).unwrap();
-    write!(output, "{}", template_html.trim().html());
+	let mut output = std::fs::File::create(output_file).unwrap();
+	write!(output, "{}", template_html.trim().html());
 }
