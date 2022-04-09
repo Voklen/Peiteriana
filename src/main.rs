f94617fb0c05efd4e1a41484855d233b2df2bb06
Alex Gorichev <Alex.Gorichev@protonmail.com>
2022-04-09 16:04:38 +0100

    Add an integration test

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ fn main() {
 	convert(markdown_file, template_file, output_file);
 }
 
-fn convert(markdown_file: &String, template_file: &String, output_file: &String) {
+fn convert(markdown_file: &str, template_file: &str, output_file: &str) {
 	use html_editor::{parse, prelude::{Editable, Htmlifiable}, Selector};
 	use markdown::file_to_html;
 	use std::io::Write;
@@ -34,7 +34,7 @@ fn convert(markdown_file: &String, template_file: &String, output_file: &String)
 		.unwrap_or_else(|err| {throw_error("write to output", output_file, err.to_string());});
 }
 
-fn throw_error(action: &str, file: &String, err: String) -> (String, Vec<html_editor::Node>, std::fs::File) { 
+fn throw_error(action: &str, file: &str, err: String) -> (String, Vec<html_editor::Node>, std::fs::File) { 
 	// All these return types are just to be able to put it in a unwrap_or_else by just indexing the tuple for the type
 	println!(
 		"{program_name}: Could not {action} file {file}: {error}",
@@ -44,4 +44,27 @@ fn throw_error(action: &str, file: &String, err: String) -> (String, Vec<html_ed
 		error = err
 	);
 	std::process::exit(1)
+}
+
+#[cfg(test)]
+mod tests {
+	#[test]
+	fn integration_test() {
+		// Imports and definitions
+		use std::fs;
+		let markdown_file = "test_data/test.md";
+		let template_file = "test_data/template.html";
+		let output_file = "test_data/output/integration_test.html";
+		let expected_result = "test_data/expected.html";
+
+		// Actual test code
+		crate::convert(markdown_file, template_file, output_file);
+		assert_eq!(
+			fs::read_to_string(output_file).unwrap(), 
+			fs::read_to_string(expected_result).unwrap()
+		);
+
+		// Clean up after test
+		fs::remove_file(output_file).unwrap();
+	}
 }
